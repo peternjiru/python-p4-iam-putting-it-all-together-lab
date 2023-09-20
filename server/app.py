063ed10b9197d43d88c92984f44fefd231b82b14
Peter Njiru <peternjirum@gmail.com>
2023-09-20 00:02:14 -0400

    3 tests remaining

--- server/app.py
+++ server/app.py
@@ -1,26 +1,131 @@
 #!/usr/bin/env python3
 
-from flask import request, session
+from flask import request, session, make_response, jsonify
 from flask_restful import Resource
 from sqlalchemy.exc import IntegrityError
 
 from config import app, db, api
 from models import User, Recipe
 
+
 class Signup(Resource):
-    pass
+    def post(self):
+        # json = request.get_json()
+        # user = User(
+        #     username=json['username'],
+        #     image_url=json['image_url'],
+        #     bio=json['bio'],
+
+        # )
+        # user.password_hash = json['password']  # Use the setter method
+        # db.session.add(user)
+        # db.session.commit()
+        # session['user_id'] = user.id
+        # return user.to_dict(), 201
+
+        json = request.get_json()
+        username = json.get('username')
+
+        if username is None or username.strip() == "":
+            return {'error': 'Invalid username'}, 422
+
+        user = User(
+            username=json['username'],
+            image_url=json['image_url'],
+            bio=json['bio'],
+
+        )
+
+        user.password_hash = json['password']  # Use the setter method
+        db.session.add(user)
+        db.session.commit()
+        session['user_id'] = user.id
+        return user.to_dict(), 201
+
 
 class CheckSession(Resource):
-    pass
+    def get(self):
+
+        user_id = session.get('user_id')
+        if user_id:
+            user = User.query.filter(User.id == user_id).first()
+            return {'id': user.id, 'username': user.username, 'image_url': user.image_url, 'bio': user.bio}, 200
+
+        return {}, 401
+
 
 class Login(Resource):
-    pass
+    def post(self):
+        username = request.get_json()['username']
+        user = User.query.filter(User.username == username).first()
+
+        if user:
+
+            password = request.get_json()['password']
+
+            if user and user.authenticate(password):
+                session['user_id'] = user.id
+                return {'id': user.id, 'username': user.username, 'image_url': user.image_url, 'bio': user.bio}, 200
+
+            return {'error': 'Invalid username or password'}, 401
+
+        return {'error': 'Invalid username or password'}, 401
+
 
 class Logout(Resource):
-    pass
+    def delete(self):
+
+        user_id = session.get('user_id')
+
+        if user_id:
+            session['user_id'] = None
+            # return make_response(jsonify({}), 204) # if using make_response and jsonify - same result
+            return {}, 204
+        # added this here because the pytest code was not cleatring the user_id
+        session['user_id'] = None
+        return {'error': 'Unauthorized'}, 401
+
 
 class RecipeIndex(Resource):
-    pass
+
+    def get(self):
+
+        recipes = Recipe.query.all()
+
+        serialized_recipes = []
+        for recipe in recipes:
+            serialized_recipes.append(recipe.to_dict())
+
+        return serialized_recipes
+
+    def post(self):
+        json = request.get_json()
+
+        user_id = session.get('user_id')
+
+        # title = json.get('title')
+
+        # if title is None or title.strip() == "":
+        #     return {'error': 'Invalid title'}, 422
+
+        # instructions = json.get('instructions')
+
+        # if instructions is None or instructions.strip() == "":
+        #     return {'error': 'Invalid instructions'}, 422
+
+        new_recipe = Recipe(
+            title=json['title'],
+            instructions=json['instructions'],
+            minutes_to_complete=json['minutes_to_complete'],
+            # also try session.get('user_id') or self.user_id later when done
+            user_id=user_id,
+        )
+
+        db.session.add(new_recipe)
+        db.session.commit()
+
+        return new_recipe.to_dict(), 201
+
 
 api.add_resource(Signup, '/signup', endpoint='signup')
 api.add_resource(CheckSession, '/check_session', endpoint='check_session')
--- server/models.py
+++ server/models.py
@@ -1,14 +1,71 @@
 from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy import CheckConstraint
 
 from config import db, bcrypt
+
 
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
 
-    pass
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable=False, unique=True)
+    _password_hash = db.Column(db.String)
+    image_url = db.Column(db.String)
+    bio = db.Column(db.String)
+
+    recipes = db.relationship('Recipe', backref='user')
+
+    @hybrid_property
+    def password_hash(self):
+        raise AttributeError("Password hashes may not be viewed.")
+
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(
+            password.encode('utf-8'))
+        self._password_hash = password_hash.decode('utf-8')
+
+    @password_hash.getter
+    def password_hash(self):
+        raise AttributeError("Password hashes may not be viewed.")        
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(
+            self._password_hash, password.encode('utf-8'))
+
+    def __repr__(self):
+        return f'User {self.username}, ID: {self.id}'
+
 
 class Recipe(db.Model, SerializerMixin):
     __tablename__ = 'recipes'
-    
-    pass
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    instructions = db.Column(db.String, nullable=False)
+    minutes_to_complete = db.Column(db.Integer)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    __table_args__ = (
+        CheckConstraint('length(instructions) >= 50', name='check_instructions_length'),
+    )
+
+    # # this code prevents the recursion error when running the RecipeIndex endpoint
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'instructions': self.instructions,
+            'minutes_to_complete': self.minutes_to_complete,
+            'user': {
+                'id': self.user.id,
+                'username': self.user.username,
+                'image_url': self.user.image_url,
+                'bio': self.user.bio,
+            }
+        }
+
+    def __repr__(self):
+        return f'Recipe {self.title}, ID: {self.id}'
